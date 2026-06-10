e666754a9ec08fe740c51778ca6f30a1ea95d0a6
larry <larryramjutton@gmail.com>
2026-06-10 12:10:06 +0200

    testing routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Welcome')->name('home');
+Route::get('/', function () {
+    return 'Laravel is alive';
+});
+
+
+/* Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
-});
+}); */
 
 require __DIR__.'/settings.php';
